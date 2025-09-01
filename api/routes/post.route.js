af7ec5922bcae49650e24d5ba926379aa6709725
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-09-01 11:18:19 +0300

    create get posts API route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verityUser.js";
 
 const router = Router();
 
 router.post('/create', verifyToken, createPost);
+router.get('/getposts', getPosts);
+
 
 export default router;
